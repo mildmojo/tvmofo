e5ee782883a708af0be077c683e0ea5f9afe58bf
mildmojo <theaterfire@yahoo.com>
2012-07-29 01:40:47 -0400

    Added a free railstheme.

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec rails server
-workers: script/delayed_job -n 2 start
+workers: script/delayed_job --sleep-delay 5 -n 1 start RAILS_ENV=production
--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,10 +1,18 @@
-class DevicesController < InheritedResources::Controller
+class DevicesController < InheritedResources::Base
   respond_to :html
 
   # POST /devices
   def create
     create! do |success, failure|
-      success.html { respond_with device, notice: 'Device created.' }
+      success.html { redirect_to devices_path, notice: 'Device created.' }
+      failure.html { redirect_to new_device_path,
+                                 notice: device.errors.full_messages.join("\n") }
+    end
+  end
+
+  def update
+    update! do |success, failure|
+      success.html { redirect_to devices_path, notice: 'Device created.' }
       failure.html { redirect_to new_device_path,
                                  notice: device.errors.full_messages.join("\n") }
     end
